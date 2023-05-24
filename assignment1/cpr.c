3129509b8f404d254a3cb7defebf81954f12cc2e
mattp.613 <sperky7933@gmail.com>
2023-05-24 12:50:33 -0400

    initial commit

---
+++ assignment1/cpr.c
@@ -0,0 +1,75 @@
+/* ------------------------------------------------ ------------
+File: cpr.c
+
+Last name: Petrucci
+Student number: 300119235
+
+Description: This program contains the code for creation
+ of a child process and attach a pipe to it.
+	 The child will send messages through the pipe
+	 which will then be sent to standard output.
+
+Explanation of the zombie process
+(point 5 of "To be completed" in the assignment):
+
+	(please complete this part);
+
+------------------------------------------------------------- */
+#include <stdio.h>
+#include <sys/select.h>
+
+/* Prototype */
+void createChildAndRead (int);
+
+/* -------------------------------------------------------------
+Function: main
+Arguments: 
+	int ac	- number of command arguments
+	char **av - array of pointers to command arguments
+Description:
+	Extract the number of processes to be created from the
+	Command line. If an error occurs, the process ends.
+	Call createChildAndRead to create a child, and read
+	the child's data.
+-------------------------------------------------- ----------- */
+
+int main (int ac, char **av)
+{
+ int processNumber; 
+ if (ac == 2)
+ {
+ if (sscanf (av [1], "%d", &processNumber)== 1)
+ {
+    createChildAndRead(processNumber);
+ }
+    else fprintf(stderr, "Cannot translate argument\n");
+ }
+    else fprintf(stderr, "Invalid arguments\n");
+    return (0);
+}
+
+
+/* ------------------------------------------------ -------------
+Function: createChildAndRead
+Arguments: 
+	int prcNum - the process number
+Description:
+	Create the child, passing prcNum-1 to it. Use prcNum
+	as the identifier of this process. Also, read the
+	messages from the reading end of the pipe and sends it to 
+	the standard output (df 1). Finish when no data can
+	be read from the pipe.
+-------------------------------------------------- ----------- */
+
+void createChildAndRead(int prcNum)
+{
+	printf("parent process number is: %d\n", (int) prcNum);
+
+	int i = prcNum -1;
+
+	for(;i > 0; i--){
+
+	printf("fork process number is: %d\n", (int) i);
+	}
+	
+}
