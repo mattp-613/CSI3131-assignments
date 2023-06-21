f9447c1b1fb4908c1f27e59c66c264e55dc75ac6
mattp.613 <sperky7933@gmail.com>
2023-06-21 11:03:41 -0400

    initial commit for assignment 3

---
+++ assignment3/assignment3.C
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+// Defines
+#define MAX_STUDENTS 10
+#define MAX_CHAIRS 3
+// Global variables
+int students_waiting = 0;
+sem_t sem_ta;
+sem_t sem_students;
+pthread_mutex_t mutex_chairs = PTHREAD_MUTEX_INITIALIZER;
+
+// Function prototypes
+void *ta_thread(void *arg);
+void *student_thread(void *arg);
+void program(int student_id);
+
+int main() {
+    pthread_t ta;
+    pthread_t students[MAX_STUDENTS];
+    int i, student_ids[MAX_STUDENTS];
+
+    // Initialize semaphores
+    sem_init(&sem_ta, 0, 0);
+    sem_init(&sem_students, 0, 0);
+
+    // Create TA thread
+    pthread_create(&ta, NULL, ta_thread, NULL);
+
+    // Create student threads
+    for (i = 0; i < MAX_STUDENTS; i++) {
+        student_ids[i] = i + 1;
+        pthread_create(&students[i], NULL, student_thread, (void *)&student_ids[i]);
+    }
+
+    // Join student threads
+    for (i = 0; i < MAX_STUDENTS; i++) {
+        pthread_join(students[i], NULL);
+    }
+
+    // Below should not happen unless a thread implodes
+
+    // Destroy semaphores and mutex (garbage collect)
+    sem_destroy(&sem_ta);
+    sem_destroy(&sem_students);
+    pthread_mutex_destroy(&mutex_chairs);
+
+    return 0;
+}
+
+void *ta_thread(void *arg) {
+    while (1) {
+        sem_wait(&sem_ta);  // Wait for a student to wake up the TA
+
+        while (1) {
+
+            pthread_mutex_lock(&mutex_chairs);
+
+            if (students_waiting > 0) {
+                sem_post(&sem_students);  // Signal student to get help
+                students_waiting--;
+                pthread_mutex_unlock(&mutex_chairs);
+
+            } 
+            
+            else {
+                pthread_mutex_unlock(&mutex_chairs);
+                break;  // No more students waiting, TA goes back to napping
+            }
+
+        }
+
+        if (sem_trywait(&sem_ta) == 0) {
+            break;  // Exit the TA thread if notified to do so
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
+void *student_thread(void *arg) {
+    int student_id = *((int *)arg);
+
+    while (1) {
+        program(student_id);  // Student programs for a period of time
+
+        pthread_mutex_lock(&mutex_chairs);
+
+        if (students_waiting < MAX_CHAIRS) {
+            students_waiting++;
+            pthread_mutex_unlock(&mutex_chairs);
+            sem_post(&sem_ta);  // Wake up the TA if sleeping
+
+            sem_wait(&sem_students);  // Wait for TA's help
+            printf("Student %d is getting help from the TA.\n", student_id);
+
+        }
+
+        else {
+            pthread_mutex_unlock(&mutex_chairs);
+            // No available chairs, student continues programming
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
+void program(int student_id) {
+    printf("Student %d is programming.\n", student_id);
+    // Simulate programming by sleeping for a random period of time
+    int sleep_time = rand() % 5 + 1;
+    sleep(sleep_time);
+}
