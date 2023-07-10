1039ae8511b198dd4f6e42eb4fd8fa0b1df2e7d9
mattp.613 <sperky7933@gmail.com>
2023-07-10 14:09:20 -0400

    find hmmm

--- assignment4/fifo_lru.py
+++ assignment4/fifo_lru.py
@@ -25,8 +25,34 @@ def fifo(string, num_page_frames):
             page[FIFOcounter] = char
             FIFOcounter+=1
 
-    return "given string:{}\nfinal page:{}\nfaults:{}.".format(string, page, fault)
+    return "--FIFO--\ngiven string:{}\nfinal page:{}\nfaults:{}.".format(string, page, fault)
+
+def lru(string, num_pages_frames):
+    faults = 0
+    page = dict()
+    for i in range(0, num_page_frames):
+        page[i] = ''
+    fault = 0
+    lru_value = 0
+    last_accessed = []
+    for i in range(0, num_page_frames):
+        last_accessed.append(0)
+    #last_accessed will have the index of the placement of the character in the string.
+    #The lower the index in the string, the higher chance it has to be replaced.
+    #Each index in this list corresponds to the key in the page dictionary.
+    for i in range(0, len(string)):
+        char = string[i]
+
+        if char not in page.values(): #if it is not already in the dictionary (skip duplicates AND already existing)
+            fault+=1
+            for y in range(0, num_page_frames): #Find the last frame accessed in the list
+                if lru_value < page.get(hmmmm): #TODO: Find hmmmm
+                    lru_value = y
+            page[lru_value] = char
+            lru_value = 0
+
+
 if __name__ == "__main__":
     string = random_page_reference_string(20)
     num_page_frames = 7
-    print(fifo(string, num_page_frames))
+    print(lru(string, num_page_frames))
