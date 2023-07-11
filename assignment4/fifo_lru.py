4a4c48966f617c6ef8c3d9c0afbf3f730170bb93
mattp.613 <sperky7933@gmail.com>
2023-07-11 10:32:18 -0400

    almost finished LRU

--- assignment4/fifo_lru.py
+++ assignment4/fifo_lru.py
@@ -34,25 +34,40 @@ def lru(string, num_pages_frames):
         page[i] = ''
     fault = 0
     lru_value = 0
-    last_accessed = []
+    lru_list = []
+    lru_index = 0
     for i in range(0, num_page_frames):
-        last_accessed.append(0)
-    #last_accessed will have the index of the placement of the character in the string.
-    #The lower the index in the string, the higher chance it has to be replaced.
-    #Each index in this list corresponds to the key in the page dictionary.
+        lru_list.append(0)
+
+    #This algorithm is sort of complicated so let me explain:
+    #Instead of having each page frame have a number for the amount of "hits" it has,
+    #It will instead correlate to the last index in the character string the same value appeared in.
+    #Therefore it will require less strain on the system.
+    #So we want to find the number in lru_list with the *lowest* value, so it correlates
+    #With the lowest index in the string, aka, the last time in the string that value appeared
     for i in range(0, len(string)):
         char = string[i]
 
         if char not in page.values(): #if it is not already in the dictionary (skip duplicates AND already existing)
             fault+=1
-            for y in range(0, num_page_frames): #Find the last frame accessed in the list
-                if lru_value < page.get(hmmmm): #TODO: Find hmmmm
-                    lru_value = y
-            page[lru_value] = char
-            lru_value = 0
+            print(lru_list)
+            for y in range(0, len(lru_list)):
+                if lru_value > lru_list[y]:
+                    lru_value = lru_list[y]
+                    lru_index = y
+                    print(lru_list)
+                    
 
+            page[lru_index] = char
+
+            #update LRU list
+            lru_list[lru_index] = i
+            #reset lru_value
+            lru_value = 0
+            #print(page)
 
 if __name__ == "__main__":
-    string = random_page_reference_string(20)
+    #string = random_page_reference_string(20)
+    string = "000202391231232321423875342754377777774744447474474"
     num_page_frames = 7
     print(lru(string, num_page_frames))
